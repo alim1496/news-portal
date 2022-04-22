83546aab7d82642b51f208c7f9326d682345705a
marzia0011 <marzia.aditi@gmail.com>
2022-04-22 20:48:16 +0600

    fix redirect

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -3,7 +3,7 @@ import { Outlet, Navigate } from "react-router-dom";
 import Auth from "../utils/auth";
 
 const ProtectedRoute = () => {
-    return Auth.isUserAuthenticated() ? <Outlet /> : <Navigate to="/login" />;
+    return Auth.isUserAuthenticated() ? <Outlet /> : <Navigate to="/admin/login" />;
 };
 
 export default ProtectedRoute;
