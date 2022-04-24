09df7d4827f3f7b5ed4a3ca39c71ff4d6b18ada4
alim1496 <alim1496@gmail.com>
2022-04-24 16:25:24 +0600

    show token for test

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Auth from "../utils/auth";
 
 const Dashboard = () => {
     return (
-        <div className="container mx-auto">Hello from Dashboard</div>
+        <div className="container mx-auto">
+            Your token is {Auth.getToken()}
+        </div>
     );
 }
 
